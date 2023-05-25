0fea51a2b1a362c70c92ffc6a9d9897d9d3e5ca3
Luda <luda50177@gmail.com>
2023-05-25 18:12:55 +0300

    add, edit, delete notes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,65 @@
+import React, {useState} from "react";
 import Header from "./components/Header/Header";
 import Workspace from "./components/Workspace/Workspace";
-import React from "react";
 
-const App = () => {
+// store
+import {DBConfig} from './DBConfig';
+import {initDB, useIndexedDB} from 'react-indexed-db';
+
+initDB(DBConfig);
+
+const App: React.FC = () => {
+    const { add, getAll, deleteRecord, update } = useIndexedDB('notes');
+
+    const [note, setNote] = useState('');
+    const [noteActiveId, setNoteActiveId] = useState();
+    const [allNotes, setAllNotes] = useState([]);
+
+
+    const changeValueNote = (text) => {
+        setNote(text);
+        update({ id: noteActiveId, title: text }).then(
+            () => {
+                getAll().then(personsFromDB => {
+                    updateListNotes(personsFromDB);
+                });
+            },
+            error => {
+                console.log(error);
+            }
+        );
+        console.log('noteActiveId', noteActiveId);
+    }
+    const updateListNotes = (allNotes) => {
+        setAllNotes(allNotes.sort((a, b) => {
+            return b.id - a.id
+        }));
+    }
+    const setActiveNote = (item) => {
+        setNoteActiveId(item.id);
+        setNote(item.title);
+    }
     return (
         <div className="App">
-            <Header />
-            <Workspace />
-        </div>
-    )
-}
+            <Header
+                note={note}
+                updateListNotes={updateListNotes}
+                changeValueNote={changeValueNote}
+                setNoteActiveId={setNoteActiveId}
+                allNotes={allNotes}
+                setActiveNote={setActiveNote}
+                noteActiveId={noteActiveId}
+            />
+            <Workspace
+                note={note}
+                changeValueNote={changeValueNote}
+                allNotes={allNotes}
+                updateListNotes={updateListNotes}
+                noteActiveId={noteActiveId}
+                setActiveNote={setActiveNote}
+            />
 
+        </div>
+    );
+};
 export default App;
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,26 +3,66 @@ import {ReactComponent as Add} from '../../svg/add.svg';
 import {ReactComponent as Basket} from '../../svg/basket.svg';
 import {ReactComponent as Edit} from '../../svg/edit.svg';
 
+// Hooks
+import {useIndexedDB} from 'react-indexed-db';
+
 // styles
 import "./MenuStyle.scss";
 
-function Menu(props) {
+function Menu({note, setActiveNote, allNotes, updateListNotes, noteActiveId, setNoteActiveId}) {
+    const { add, getAll, deleteRecord, update } = useIndexedDB('notes');
+    console.log(allNotes);
+    const handleAddNote = () => {
+        add({ title: '' }).then(
+            (id, z) => {
+                setActiveNote({ id, title: '' });
+                console.log('id', id);
+            },
+            error => {
+                console.log(error);
+            }
+        );
+        getAll().then(personsFromDB => {
+            updateListNotes(personsFromDB);
+        });
+    }
+    const handleDeleteNote = () => {
+        deleteRecord(noteActiveId).then(() => {
+            // console.log(event.target);
+        });
+        getAll().then(personsFromDB => {
+            updateListNotes(personsFromDB);
+        });
+    }
+    const handleEditNote = () => {
+        // update({title: note},noteActiveValue).then(event => {
+        //     changeValueNote(noteActiveValue);
+        // });
+        // getAll().then(personsFromDB => {
+        //     updateListNotes(personsFromDB);
+        // });
+    }
     return (
         <div className='menu'>
             <div className='menu__list'>
                 <button
+                    onClick={handleAddNote}
                     type='text'
                     className='menu__item'
                 >
                     <Add />
                 </button>
                 <button
+                    disabled={allNotes.length === 0}
+                    onClick={handleDeleteNote}
                     type='text'
                     className='menu__item'
                 >
                     <Basket />
                 </button>
                 <button
+                    disabled={allNotes.length === 0}
+                    onClick={handleEditNote}
                     type='text'
                     className='menu__item'
                 >
@@ -30,6 +70,7 @@ function Menu(props) {
                 </button>
             </div>
         </div>
+
     )
 }
 
